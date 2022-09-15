8610ce3270d7c75470f9193c924df567942f0884
Jil Henry <112335895+jilhenrywave@users.noreply.github.com>
2022-09-16 00:15:15 +0100

    Revert "Feature: User Account Creation"

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-console */
 const express = require('express');
-require('./db/sequelize');
-const userRouter = require('./user/user.router');
 
 const app = express();
 
 app.use(express.json());
-app.use(userRouter);
 
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
   res.send('StackoverflowLite Project API');
 });
 
--- user/user.controller.js
+++
@@ -1,8 +0,0 @@
-const registerUserService = require('./user-services/register-user');
-
-const registerUser = async (payload) => {
-  const serviceResponse = await registerUserService(payload);
-  return { statusCode: serviceResponse.code || 201, body: { ...serviceResponse } };
-};
-
-module.exports = { registerUser };
