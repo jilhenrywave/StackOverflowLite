75dece12c45769632b5cbd6ec3281389f5b106a3
Jil Henry <jil.henry@flutterwavego.com>
2022-09-19 00:23:12 +0100

    fix: Get question sort query validator case sensitivity

--- question/validators/get-question.validator.js
+++ question/validators/get-question.validator.js
@@ -5,10 +5,11 @@ const { isValidID, isNumber } = require('../../util/field-validators');
 
 const getQuestionValidator = ({ ownerId = '', start = 0, limit = 0, sort = '' }) => {
   const validatorError = new ValidationError();
+  const qSort = sort.toUpperCase();
   if (ownerId && !isValidID(ownerId)) validatorError.addErrorMessage(ERROR_MESSAGE.invalidID);
   if (start && !isNumber(start)) validatorError.addErrorMessage('Query field, start, is not a number');
   if (limit && !isNumber(limit)) validatorError.addErrorMessage('Query field, limit, is not a number');
-  if (sort && (sort !== 'DESC' && sort !== 'ASC')) {
+  if (sort && (qSort !== 'DESC' && qSort !== 'ASC')) {
     validatorError.addErrorMessage('Invalid value for query field - sort. Accepted values are ASC or DESC');
   }
 
