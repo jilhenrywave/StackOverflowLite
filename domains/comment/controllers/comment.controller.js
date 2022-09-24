cda3ce3ea19fe8401bb0670c7d84da47b429bf13
Jil Henry <jil.henry@flutterwavego.com>
2022-09-24 18:50:40 +0100

    feature: Edit Comment implemented

--- domains/comment/controllers/comment.controller.js
+++ domains/comment/controllers/comment.controller.js
@@ -1,9 +1,16 @@
 const postCommentService = require('../services/post-comment');
 const getCommentsService = require('../services/get-comments');
+const updateCommentService = require('../services/update-comment');
 const serviceResponseHandler = require('../../../util/service-handlers/service-response-handler');
 
 const postComment = async (payload) => serviceResponseHandler(payload, postCommentService, 201);
 
 const getComments = async (payload) => serviceResponseHandler(payload, getCommentsService, 200);
 
-module.exports = { postComment, getComments };
+const updateComment = async (payload) => serviceResponseHandler(payload, updateCommentService, 200);
+
+module.exports = {
+  postComment,
+  getComments,
+  updateComment,
+};
---
+++ domains/comment/services/update-comment.js
@@ -0,0 +1,56 @@
+const { Comment } = require('../../../db/model-handler');
+const { includeUser } = require('../../../db/query-helper/include-query-constants');
+const { ServerError, RequestError } = require('../../../util/error-handlers');
+const QueryBuilder = require('../../../db/query-helper/QueryBuilder');
+const { ERROR_MESSAGE } = require('../../../util/constants');
+const serviceErrorHandler = require('../../../util/service-handlers/services-error-handler');
+
+/**
+ * Retrieves comment record from database
+ * @param {string} id
+ * @returns {object}
+ * @throws
+ */
+const getComment = async (id) => {
+  const query = new QueryBuilder()
+    .setModel(Comment)
+    .setAttributes({ exclude: ['ownerId'] })
+    .setInclude([includeUser])
+    .setNest(true)
+    .setSubQuery(false)
+    .build();
+
+  const comment = await query.execFindByPk(id);
+
+  if (!comment) throw new ServerError();
+
+  return comment;
+};
+
+/**
+ * Updates Comment record on database
+ * @param {object} updateEntry
+ * @returns {object} updatedAnswer
+ */
+
+const updateComment = async ({ paramId = '', commentBody = '', user }) => {
+  try {
+    if (!paramId || !commentBody) throw new ServerError();
+
+    const query = new QueryBuilder()
+      .setModel(Comment)
+      .setWhere({ id: paramId, ownerId: user.id })
+      .build();
+
+    const affectedRecords = await query.execUpdate({ body: commentBody });
+
+    if (affectedRecords[0] < 1) throw new RequestError(422, ERROR_MESSAGE.updateError);
+
+    return await getComment(paramId);
+  } catch (e) {
+    console.log(e);
+    return serviceErrorHandler(e);
+  }
+};
+
+module.exports = updateComment;
