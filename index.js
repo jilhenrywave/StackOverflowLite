32df348aa07f891c5784643efb3ec03e6d57a724
Jil Henry <jil.henry@flutterwavego.com>
2022-09-30 17:21:37 +0100

    fix: Handle error thrown by json middleware

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 /* eslint-disable no-console */
 const express = require('express');
+const { RequestError } = require('./util/error-handlers');
 const v1Router = require('./versions/v1.router');
 require('./db/sequelize');
 require('./db/associations');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json(), (err, _req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    res.status(400).send(new RequestError(400, err.message));
+  } else next();
+});
+
 app.use('/api/v1', v1Router);
 
 app.all('*', (_req, res) => {
